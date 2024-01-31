00c66c20177749163f59155802fbeada0e6055cb
Jason Wu <jjw73012@gmail.com>
2024-01-31 15:40:18 -0600

    Sending Flask Backend and React Frontend changes

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/members")
 def members():
-    return{"members":["Member1","Mem2", "test"]}
+    return{"members":["Member1","Mem2"]}
 
 if __name__ == "__main__":
     app.run(debug=True)
