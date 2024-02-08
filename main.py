1bd1df7c5d29ef83323fb4269cad36ec42cdb1b5
Jason_Wu3 <jason_wu3@baylor.edu>
2024-02-08 16:48:37 -0600

    Tokenizer running in main. TODO: send tokenizer from main into its own function

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import io
+
+from LanguageTokenizer.TxtToToken import text_tokenize
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -11,6 +15,26 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    ## TODO send this stuff to a function and run it
     print_hi('PyCharm')
+    file = io.open("DBTextFiles/eng_ENGESV.api.txt", mode="r", encoding="utf-8")
+    tokens = []
+    tokens.append(text_tokenize(file, "eng"))
+    file = io.open("DBTextFiles/cmn_CHNNCT.cloud.txt", mode="r", encoding="utf-8")
+    tokens.append(text_tokenize(file,"cmn"))
+
+    length = len(tokens)
+    mapping = []
+    for i in range(length):
+        for j in range(len(tokens[i])):
+            mapping.append(
+                {tokens[i][j].get('lang') : tokens[i][j].get('tl'),
+                 tokens[(i+1) % length][j].get('lang') : tokens[(i+1) % length][j].get('tl')}
+            )
+
+    for mapped in mapping:
+        print(mapped.__str__())
+
+    file.close()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
