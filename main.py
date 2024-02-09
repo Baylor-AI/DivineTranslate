d061f0d4cc1a52f1ea1995edadbff67068d613a7
Jason_Wu3 <jason_wu3@baylor.edu>
2024-02-09 14:54:38 -0600

    test commit

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
 import os, glob
 
 # This is the directory where all the txt files should go for tokenization.
-txt_directory='DBTextFiles'
-txt_directory_size=len(txt_directory)+1
-token_directory='TokenizedDB'
+txt_directory = 'DBTextFiles'
+token_directory = 'TokenizedDB'
+txt_directory_size = len(txt_directory) + 1
+extension_len = 4
+lang_len = 3
+
 
 def get_all_tokened():
     tokens = []
-    extension_len=4
-    lang_len=3
     for filename in glob.glob(os.path.join(txt_directory, '*.txt')):
         with io.open(os.path.join(os.getcwd(), filename), mode='r', encoding='utf-8') as file:
-            print(filename[txt_directory_size:txt_directory_size+lang_len])
-            content = text_tokenize(file, filename[txt_directory_size:txt_directory_size+lang_len])
+            print(filename[txt_directory_size:txt_directory_size + lang_len])
+            content = text_tokenize(file, filename[txt_directory_size:txt_directory_size + lang_len])
             content.append(filename[txt_directory_size:-extension_len])
             tokens.append(content)
 
     num_tokens = len(tokens)
     list_len = len(tokens[0]) - 1
     for i in range(num_tokens):
+        # checks if the translation is blank
+        blank_check = False
         for k in range(num_tokens):
             mapping = []
             if i != k:
-                with io.open(os.path.join(os.path.join(os.getcwd()), token_directory, f'{tokens[i][list_len]}_to_{tokens[k][list_len]}.txt'),
-                             mode='w', encoding='utf-8') as output:
-                    # mapping.append({'langTolang':f'{tokens[i][list_len]}_to_{tokens[k][list_len]}'})
+                with io.open(os.path.join(os.path.join(os.getcwd()), token_directory,
+                                          f'{tokens[i][list_len]}_to_{tokens[k][list_len]}.txt'),
+                                            mode='w', encoding='utf-8') as output:
                     for j in range(list_len):
+
+                        if tokens[i][j].get('tl')[:-2] is None or tokens[k][j].get('tl')[:-2] is None:
+                            blank_check = True
+                            break
                         mapping.append(
                             {tokens[i][j].get('lang'): tokens[i][j].get('tl'),
                              tokens[k][j].get('lang'): tokens[k][j].get('tl')}
@@ -33,9 +40,13 @@ def get_all_tokened():
                     for mapped in mapping:
                         # print(mapped.__str__())
                         output.write(f'{mapped.__str__()}\n')
+            if blank_check:
+                break
+
 
 import io
 from LanguageTokenizer.TxtToToken import text_tokenize
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -50,7 +61,5 @@ def print_hi(name):
 if __name__ == '__main__':
     get_all_tokened()
     print_hi('PyCharm')
-
-
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
