3cf25f325b7e4486bacb04f5cb2a3af8416e9706
Jason_Wu3 <jason_wu3@baylor.edu>
2024-02-16 15:21:16 -0600

    Wordnet Demo work list TODO: grab only synsets with the exact word TODO: make comparisons based on part of speech

--- Wordnet/wordnet_test.py
+++ Wordnet/wordnet_test.py
@@ -1,15 +1,14 @@
 import nltk
 nltk.download('popular')
-from nltk.corpus import wordnet
+# set abbreviations for the wordnet items
+from nltk.corpus import wordnet as wn
+from nltk.stem import WordNetLemmatizer as wnl
 
 def synset_program():
-    syns = wordnet.synsets("program")
+    syns = wn.synsets("program")
     for syn in syns:
         print(f'=============\nWord: {syn.name()}')
-        print(f'Lemmas: {syn.lemmas()}')
-        # for l in syn.lemmas():
-        #     print(f'{l.name()},'),
-
+        print(f'Lemmas: {set(syn.lemmas())}')
         print(f'\nDefinition: {syn.definition()}')
         i=0
         print(f'Sentence Examples:')
@@ -17,12 +16,10 @@ def synset_program():
             print(f'\t{++i}. {ex}')
         print("=============")
 
-
 def synset_choose(choice):
     synonyms=[]
     antonyms=[]
-
-    for syn in wordnet.synsets(str(choice)):
+    for syn in wn.synsets(str(choice)):
         for l in syn.lemmas():
             synonyms.append(l.name())
             if l.antonyms():
@@ -30,7 +27,31 @@ def synset_choose(choice):
     if synonyms: print(f'Synonyms: {set(synonyms)}')
     if antonyms: print(f'Antonyms: {set(antonyms)}')
 
-def synset_compare(choice, compare_with):
-    print(f'{wordnet.synsets(choice)[0]} v {wordnet.synsets(compare_with)[0]}')
-    if wordnet.synsets(choice) and wordnet.synsets(compare_with):
-        print(wordnet.synsets(choice)[0].wup_similarity(wordnet.synsets(compare_with)[0]))
+def synset_compare(choice, compare_with, pos=None):
+    lem = wnl()
+    # choice = lem.lemmatize(choice)
+    # compare_with = lem.lemmatize(compare_with)
+    choice = wn.morphy(choice)
+    compare_with = wn.morphy(compare_with)
+    print(f"after lemma: {choice} v {compare_with}")
+    print(f'{set(wn.synsets(choice))}\n v \n{set(wn.synsets(compare_with))}')
+    if wn.synsets(choice) and wn.synsets(compare_with):
+        print(wn.synsets(choice)[0].wup_similarity(wn.synsets(compare_with)[0]))
+
+def synset_speech_match(choice, compare_with):
+    lem = wnl()
+
+# gets only synsets that contain the exact word
+def synset_get_exact(to_synset):
+    '''
+    Gets only synsets that contain the original word
+    :param to_synset: the word to get the exact synsets for
+    :return: a set of synsets that only contain the input word
+    '''
+    lem=wnl()
+    to_synset=wn.morphy(to_synset)
+    exact_word = []
+    for syn in wn.synsets(to_synset):
+        if to_synset in syn.name():
+            exact_word.append(syn)
+    return exact_word
