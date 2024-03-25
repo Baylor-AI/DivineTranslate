c17c68002d8f90dfe7f3bae334e4b95756fa067a
lauren_adams3 <lauren_adams3@baylor.edu>
2024-03-25 15:35:42 -0500

    fixed broken model page with last git pull

--- ModelAPI/main.py
+++ ModelAPI/main.py
@@ -14,12 +14,6 @@ from transformers import AutoTokenizer, T5ForConditionalGeneration
 #   "target_lang_code": "cmn"
 # }
 
-
-
-
-
-
-
 app = FastAPI()
 
 app.add_middleware(
@@ -30,33 +24,36 @@ app.add_middleware(
 )
 
 
-padding = "max_length"
-truncation = True
-max_length = 512
-
-# Load pre-trained model
-trained_model = T5ForConditionalGeneration.from_pretrained('./')
-trained_model.to('cuda')
-
 class TranslationRequest(BaseModel):
     text: str
     source_lang_code: str
     target_lang_code: str
 
-def generate_prefix(source_lang_code, target_lang_code):
-    return f"translate {source_lang_code} to {target_lang_code}: "
-
 @app.post("/translate/")
 async def translate_text(request: TranslationRequest):
-    prefix = generate_prefix(request.source_lang_code, request.target_lang_code)
-    prefixed_text = prefix + request.text
+    # Define tokenization and model loading within the endpoint function
+    padding = "max_length"
+    truncation = True
+    max_length = 512
 
     tokenizer = AutoTokenizer.from_pretrained('google/byt5-small')
+    trained_model = T5ForConditionalGeneration.from_pretrained('./')
 
+    prefix = f"translate {request.source_lang_code} to {request.target_lang_code}: "
+    prefixed_text = prefix + request.text
+
+    # Tokenize the input text
     encoded_input = tokenizer(prefixed_text, padding=padding, truncation=truncation, max_length=max_length,
-                              return_tensors="pt").to('cuda')
+                              return_tensors="pt")
 
-    output_tokens = trained_model.generate(**encoded_input, max_length=max_length).to('cuda')
+    # Generate translation
+    output_tokens = trained_model.generate(**encoded_input, max_length=max_length)
+
+    # Decode the generated tokens to text
     translated_text = tokenizer.decode(output_tokens[0], skip_special_tokens=True)
 
     return {"translated_text": translated_text}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
