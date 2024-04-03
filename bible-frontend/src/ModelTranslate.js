894c6f8c4dca481398abfe91e1e0bc73a9f2cf80
lauren_adams3 <lauren_adams3@baylor.edu>
2024-04-03 15:04:59 -0500

    add to git

---
+++ bible-frontend/src/ModelTranslate.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8000/translate/';
+
+const translateTextM = async (text, source_lang_code, target_lang_code) => {
+    console.log(text + " " + source_lang_code + " " + target_lang_code);
+  const response = await axios.post(
+    API_URL,
+    {
+      text: text,
+      source_lang_code: source_lang_code,
+      target_lang_code: target_lang_code,
+    }
+  );
+
+  return response.data.translated_text;
+};
+
+export default translateTextM;
