4791419bac4ba01d892cebf272edb82d91a24afd
corinneaars <corinne.aars@gmail.com>
2024-04-23 23:13:34 -0500

    Adding script to enable longpaths

--- WIN_install_reqs.sh
+++ WIN_install_reqs.sh
@@ -31,26 +31,40 @@ fi
 echo "[CHECKING] Checking length of file path..."
 current_path=$(pwd)
 path_length=${#current_path}
-max_path_length=260  # typical Windows MAX_PATH limit
+max_path_length=260
 
-if [ $path_length -ge $max_path_length ]; then
-    echo "[WARNING] The current path length is $path_length characters."
-    echo "This might cause problems with path length limitations in Windows."
-    echo "Consider moving your project to a shorter path, such as C:/Projects/"
-    echo "Alternatively, you can enable long path support in Windows 10/11:"
-    echo "1. Press Win + R, type 'regedit', and press Enter."
-    echo "2. Navigate to: Computer\\HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\FileSystem"
-    echo "3. Right-click on 'FileSystem', select New -> DWORD (32-bit) Value, name it 'LongPathsEnabled'."
-    echo "4. Set the value of 'LongPathsEnabled' to 1."
-    echo "5. Restart your computer."
-    exit 1
-fi
-echo "[SUCCESS] File path length is acceptable, proceeding with install"
+echo "Enabling long paths..."
+#pwsh
+pwsh Start-Process powershell -Verb runAs New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" -Name "LongPathsEnabled" -Value 1 -PropertyType DWORD -Force
+echo "[SUCCESS] Long paths enabled"
+
+# if [ $path_length -ge $max_path_length ]; then
+    
+#     echo "[WARNING] The current path length is $path_length characters."
+#     echo "This might cause problems with path length limitations in Windows."
+#     echo "Consider moving your project to a shorter path, such as C:/Projects/"
+#     echo "Alternatively, you can enable long path support in Windows 10/11:"
+#     echo "1. Press Win + R, type 'regedit', and press Enter."
+#     echo "2. Navigate to: Computer\\HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\FileSystem"
+#     echo "3. Right-click on 'FileSystem', select New -> DWORD (32-bit) Value, name it 'LongPathsEnabled'."
+#     echo "4. Set the value of 'LongPathsEnabled' to 1."
+#     echo "5. Restart your computer."
+#     exit 1
+# fi
+# echo "[SUCCESS] File path length is acceptable, proceeding with install"
 
 # Install requirements
+echo "[INSTALL] Installing requirements.txt..."
 pip install -r requirements.txt
+echo "[COMPLETE] Installed requirements.txt"
+
+echo "[INSTALL] Installing react-scripts..."
 npm install react-scripts --save
+echo "[COMPLETE] Installed react-scripts"
+
+echo "[INSTALL] Installing boostrap..."
 npm install bootstrap --save
+echo "[COMPLETE] Installed boostrap"
 
 echo ""
 echo "[COMPLETED] Setup completed successfully..."
